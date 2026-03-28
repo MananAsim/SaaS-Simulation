9292c3f2b37dec22ec38b76c88ea9fb03855703c
Manan Asim <manansama17@gmail.com>
2026-03-28 19:38:08 +0500

    fixed next config properly

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client', 'bcryptjs'],
-};
-const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
 export default nextConfig;
